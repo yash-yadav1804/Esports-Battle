e9d22f2adc02d0ccbb72502e96ff3cdf48999164
Yash Yadav <yashkumaryadav510@gmail.com>
2026-06-19 12:46:04 +0530

    feat: Add MatchRegistration Module

---
+++ server/models/MatchRegistration.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const matchRegistrationSchema = new mongoose.Schema(
+  {
+    player: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    team: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Team",
+      required: true,
+    },
+
+    matchRoom: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "MatchRoom",
+      required: true,
+    },
+
+    bgmiName: {
+      type: String,
+      required: true,
+    },
+
+    bgmiId: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("MatchRegistration", matchRegistrationSchema);
